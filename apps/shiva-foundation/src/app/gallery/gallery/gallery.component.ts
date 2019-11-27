5cc698ecc385b7b7aca4d530fd652392a3d0fb12
vkpindia <infovkpg@gmail.com>
2019-11-27 22:10:15 +0530

    SF Gallery Under Development

---
+++ apps/shiva-foundation/src/app/gallery/gallery/gallery.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit, Renderer2, ElementRef, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Component({
+  selector: 'shivafdn-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss']
+})
+
+export class GalleryComponent implements OnInit {
+  public slideIndex = 1;
+  constructor(private renderer: Renderer2
+    ,private elRef: ElementRef
+    ,@Inject(DOCUMENT) private document: Document) { }
+
+  ngOnInit() {
+    this.showSlides(this.slideIndex);
+  }
+
+  /**
+   * @description
+   * @author Virendra Pandey
+   * @date 2019-11-26
+   * @param {*} n
+   * @memberof GalleryComponent
+   */
+  plusSlides(n) {
+    this.showSlides((this.slideIndex += n));
+  }
+
+  /**
+   * @description
+   * @author Virendra Pandey
+   * @date 2019-11-26
+   * @param {*} n
+   * @memberof GalleryComponent
+   */
+  currentSlide(n) {
+    this.showSlides((this.slideIndex = n));
+  }
+
+  /**
+   * @description
+   * @author Virendra Pandey
+   * @date 2019-11-26
+   * @param {*} n
+   * @memberof GalleryComponent
+   */
+  showSlides(n) {
+    let i: number;
+    const slides = this.document.getElementsByClassName("mySlides");
+    const dots = this.document.getElementsByClassName("demo");
+    const captionText = this.document.getElementsByClassName("caption");
+    if (n > slides.length) {
+      this.slideIndex = 1;
+    }
+    if (n < 1) {
+      this.slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[this.slideIndex - 1].style.display = "block";
+    dots[this.slideIndex - 1].className += " active";
+    // captionText.innerHTML = dots[this.slideIndex - 1].alt;
+  }
+}
